cdf9d1cdd5829c44bf6ac32cfd3921bc722ca2e9
liecy <rahmianisa27@gmail.com>
2023-06-15 23:17:15 +0700

    add content and fix colors

--- app/src/main/java/com/example/platformlayout/CustomListViewActivity.java
+++ app/src/main/java/com/example/platformlayout/CustomListViewActivity.java
@@ -20,17 +20,23 @@ public class CustomListViewActivity extends AppCompatActivity {
 
         customListView = findViewById(R.id.customListView);
 
-        Konten konten1 = new Konten(R.drawable.img1, "10/03/2023", "Rahmi Anisa", getResources().getString(R.string.loremipsum));
-        Konten konten2 = new Konten(R.drawable.img1, "11/03/2023", "Rahmi Anisa", getResources().getString(R.string.loremipsum));
-        Konten konten3 = new Konten(R.drawable.img1, "12/03/2023", "Rahmi Anisa", getResources().getString(R.string.loremipsum));
-        Konten konten4 = new Konten(R.drawable.img1, "13/03/2023", "Rahmi Anisa", getResources().getString(R.string.loremipsum));
-        Konten konten5 = new Konten(R.drawable.img1, "14/03/2023", "Rahmi Anisa", getResources().getString(R.string.loremipsum));
+        Konten konten1 = new Konten(R.drawable.img01, "10/03/2023", "Anna of Arendelle", getResources().getString(R.string.loremipsum));
+        Konten konten2 = new Konten(R.drawable.img02, "11/03/2023", "Moana Waialiki", getResources().getString(R.string.loremipsum));
+        Konten konten3 = new Konten(R.drawable.img03, "12/03/2023", "Cinderella", getResources().getString(R.string.loremipsum));
+        Konten konten4 = new Konten(R.drawable.img04, "13/03/2023", "Belle", getResources().getString(R.string.loremipsum));
+        Konten konten5 = new Konten(R.drawable.img05, "14/03/2023", "Vanellope von Schweetz", getResources().getString(R.string.loremipsum));
+        Konten konten6 = new Konten(R.drawable.img06, "15/03/2023", "Rapunzel", getResources().getString(R.string.loremipsum));
+        Konten konten7 = new Konten(R.drawable.img07, "16/03/2023", "Princess Aurora", getResources().getString(R.string.loremipsum));
+        Konten konten8 = new Konten(R.drawable.img08, "17/03/2023", "Queen Ariel", getResources().getString(R.string.loremipsum));
 
         listKonten.add(konten1);
         listKonten.add(konten2);
         listKonten.add(konten3);
         listKonten.add(konten4);
         listKonten.add(konten5);
+        listKonten.add(konten6);
+        listKonten.add(konten7);
+        listKonten.add(konten8);
 
         CustomAdapter adapter = new CustomAdapter(this, R.layout.single_list_item, listKonten);
         customListView.setAdapter(adapter);
